63dcd337c9b6ad12593335d6c083958726342156
Jamie Matthews <jamie@dabapps.com>
2021-12-03 19:30:22 +0000

    Reformat code

--- simple_robots/tests/tests.py
+++ simple_robots/tests/tests.py
@@ -3,18 +3,19 @@ from django.test.utils import override_settings
 
 
 class RobotTestCase(TestCase):
-
     def test_deny_all(self):
-        response = self.client.get('/robots.txt')
-        self.assertEqual(response['Content-Type'], 'text/plain')
+        response = self.client.get("/robots.txt")
+        self.assertEqual(response["Content-Type"], "text/plain")
         self.assertEqual(response.content, b"User-agent: *\nDisallow: /\n")
 
-    @override_settings(ROBOTS_ALLOW_HOST='test.com', ALLOWED_HOSTS=['test.com'])
+    @override_settings(ROBOTS_ALLOW_HOST="test.com", ALLOWED_HOSTS=["test.com"])
     def test_allow_if_host_matches(self):
-        response = self.client.get('/robots.txt', HTTP_HOST="test.com")
+        response = self.client.get("/robots.txt", HTTP_HOST="test.com")
         self.assertEqual(response.content, b"User-agent: *\nAllow: /\n")
 
-    @override_settings(ROBOTS_ALLOW_HOST='test.com', ALLOWED_HOSTS=['test.com', 'somethingelse.com'])
+    @override_settings(
+        ROBOTS_ALLOW_HOST="test.com", ALLOWED_HOSTS=["test.com", "somethingelse.com"]
+    )
     def test_deny_if_host_does_not_match(self):
-        response = self.client.get('/robots.txt', HTTP_HOST="somethingelse.com")
+        response = self.client.get("/robots.txt", HTTP_HOST="somethingelse.com")
         self.assertEqual(response.content, b"User-agent: *\nDisallow: /\n")
--- simple_robots/tests/urls.py
+++ simple_robots/tests/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
 from simple_robots.views import serve_robots
 
-
 urlpatterns = [
-    url(r'robots.txt', serve_robots),
+    url(r"robots.txt", serve_robots),
 ]
